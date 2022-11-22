462335fad3d1f5d8a1fb1b7bfd41ec4bb47a4174
gsipes00 <gsipes00@yahoo.com>
2022-11-22 14:24:26 -0600

    use state array edits

--- src/data.js
+++ src/data.js
@@ -1,6 +1,7 @@
 export const data = [
-  { id: 1, name: 'john' },
-  { id: 2, name: 'peter' },
-  { id: 3, name: 'susan' },
-  { id: 4, name: 'anna' },
-];
+  { id: 1, name: 'john', age: 25 },
+  { id: 2, name: 'peter', age: 23 },
+  { id: 3, name: 'susan', age: 16 },
+  { id: 4, name: 'anna', age: 39 },
+  { id: 5, name: 'Gene', age: 55 }
+]
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -4,18 +4,22 @@ import { data } from "../../../data";
 const UseStateArray = () => {
   // instead of importing, call React.userState.
   const [people, setPeople] = React.useState(data);
+
+  // onClick function
   const removeItem = (id) => {
     let newPeople = people.filter((person) => person.id !== id);
     setPeople(newPeople);
   };
+  // return jsx
   return (
     <React.Fragment>
       {people.map((person) => {
-        const { id, name } = person;
+        const { id, name, age } = person;
         console.log(person);
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
+            <h6>Age: {age}</h6>
             <button className='btn' onClick={() => removeItem(id)}>
               remove
             </button>
